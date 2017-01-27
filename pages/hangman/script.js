670d3bb12a22edfac02ef4c66e96da8d024f45ac
FryderykG <eszkimos@gmail.com>
2017-01-27 13:47:06 +0100

    add hangman version 0.2

--- pages/hangman/script.js
+++ pages/hangman/script.js
@@ -4,9 +4,12 @@
 
 // HANGMAN OBJECT
 var hangman = {
-    alphabet: "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż",
+    alphabet : "",
     word: "",
     words: [],
+    words_obj: {},
+    categories: "",
+    category: "",
     word_hidden: "",
     language: "",
     mistake_number: 0,
@@ -33,7 +36,7 @@ var hangman = {
     },
 
     incrementScore: function() {
-        this.score++;
+        this.score += this.remaining_chances;
     },
 
     resetScore: function() {
@@ -41,14 +44,23 @@ var hangman = {
     },
 
     // WORD METHOD
-    /** Set random word from words */
-    setWord: function() {
-        this.word = this.words[Math.floor(Math.random() * this.words.length)].toLowerCase();
+    setCategory: function() {
+        /** Category list */
+        this.categories = Object.getOwnPropertyNames(this.words_obj);
+        /** Pick one category from categories */
+        this.category = this.categories[Math.floor(Math.random() * this.categories.length)];
     },
 
-    /** Display hidden word on screen*/
-    displayHiddenWord: function() {
-        document.getElementById("secretWord").innerHTML = '<p>' + this.word_hidden + '</p>';
+    displayCategory: function() {
+        document.getElementById("category").innerHTML = '<p>' + this.category.toUpperCase() + '</p>';
+    },
+
+    setWord: function() {
+        /** list of all words */
+        this.words = this.words_obj[this.category];
+        /** Pick ons word from words*/
+        this.word = this.words[Math.floor(Math.random() * this.words.length)].toLowerCase();
+        // this.word = this.words[Math.floor(Math.random() * this.words.length)].toLowerCase();
     },
 
     getWordLength: function() {
@@ -64,6 +76,11 @@ var hangman = {
                 this.word_hidden += " ";
             }
         }
+    },
+
+    /** Display hidden word on screen*/
+    displayHiddenWord: function() {
+        document.getElementById("secretWord").innerHTML = '<p>' + this.word_hidden + '</p>';
     },
 
     /** Return letter at position number */
@@ -100,20 +117,26 @@ var hangman = {
         var picture_id = "pic" + this.mistake_number;
         if (this.mistake_number >= 9) {
             this.resetScore();
+            // Remove hidden class from picture
             document.getElementById(picture_id).className = "";
+            // Hide alphabet on the end of the game
             document.getElementById("alphabet").className += "hidden";
+            // Remove hidden class from message_lost
             document.getElementById("message_lost").className = "";
+            // When player lost display correct word
+            this.setWordHidden(this.getWord());
+            // this.displayHiddenWord()
         } else {
             document.getElementById(picture_id).className = "";
             if (this.mistake_number === 4) {
-                document.getElementById("pic3").className = "opacityZero";
+                document.getElementById("pic3").className = "opacity-zero";
             }
         }
     },
 
     hidePictures: function() {
       for (var i = 1; i < 10; i++){
-          document.getElementById("pic" + i).className = "opacityZero";
+          document.getElementById("pic" + i).className = "opacity-zero";
       }
     },
 
@@ -165,8 +188,8 @@ function addListeners() {
     }
 }
 // ADD EventListeners at script start
-document.getElementById("flag_pol").addEventListener("click", languageSelect);
-document.getElementById("flag_uk").addEventListener("click", languageSelect);
+document.getElementById("flag_pl").addEventListener("click", languageSelect);
+document.getElementById("flag_gb").addEventListener("click", languageSelect);
 document.getElementById("button_lost").addEventListener("click", start);
 document.getElementById("button_win").addEventListener("click", start);
 
@@ -176,7 +199,7 @@ function check() {
     var letterInMessage = false;
 
     this.removeEventListener("click", check);
-
+    // Check if clicked letter is in word
     for(var i = 0; i < hangman.getWordLength(); i++) {
         //  Compare word letter at position "i" to alphabet letter at position "letter_number"
         if (hangman.getWordCharAt(i) === hangman.getAlphabetLetter(letter_number)) {
@@ -190,14 +213,21 @@ function check() {
         hangman.playSound("yes");
         //Change color to green
         document.getElementById("letter" + letter_number).className += " correct";
-        // Disable onclick function
-        document.getElementById("letter" + letter_number).setAttribute("onclick",";");
+
+        // Check if player find the answer
+        if (hangman.getWord() === hangman.getWordHidden()) {
+            hangman.incrementScore();
+            hangman.displayScore();
+            /** hide alphabet*/
+            document.getElementById("alphabet").className += "hidden";
+            /** show message win */
+            document.getElementById("message_win").className = "";
+        }
+
     } else {
         hangman.playSound("no");
         // Change color to red
         document.getElementById("letter" + letter_number).className += " wrong";
-        // Disable onclick function
-        document.getElementById("letter" + letter_number).setAttribute("onclick",";");
         // Add one to mistake number
         hangman.incrementMistakeNumber();
         // Refresh picture
@@ -208,15 +238,7 @@ function check() {
     hangman.displayHiddenWord();
     hangman.displayRemainingChances();
 
-    // Check if player find the answer
-    if (hangman.getWord() === hangman.getWordHidden()) {
-        hangman.incrementScore();
-        hangman.displayScore();
-        /** hide alphabet*/
-        document.getElementById("alphabet").className += "hidden";
-        /** show message win */
-        document.getElementById("message_win").className = "";
-    }
+
 }
 
 // LOAD WORDS LIST FROM JSON
@@ -249,31 +271,35 @@ function changeClassesName(elements, addOrRemove, newClassName) {
 function languageSelect() {
     document.getElementById("header").className = "hide";
     document.getElementById("board").className = "";
-    var elements_uk = document.getElementsByClassName("lang_uk");
-    var elements_pl = document.getElementsByClassName("lang_pl");
+    var elements_en = document.getElementsByClassName("en_gb");
+    var elements_pl = document.getElementsByClassName("pl_pl");
 
-    if (this.id === "flag_pol") {
-        hangman.language = "pol";
+    if (this.id === "flag_pl") {
+        hangman.language = "pl";
         // HIDE ELEMENTS IN ENGLISH
-        changeClassesName(elements_uk, "add", "hidden");
+        changeClassesName(elements_en, "add", "hidden");
         changeClassesName(elements_pl, "remove", "hidden");
         document.getElementById("slash").className = "hidden";
+        // SET POLISH ALPHABET
+        hangman.alphabet = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż";
         // LOAD POLISH WORDS
         loadJSON("./words.json", function(text){
-            hangman.words = JSON.parse(text).words.pol;
+            hangman.words_obj = JSON.parse(text).words.pl;
             // START THE GAME
             start();
         });
 
-    } else if (this.id === "flag_uk") {
-        hangman.language = "uk";
+    } else if (this.id === "flag_gb") {
+        hangman.language = "en";
         // HIDE ELEMENTS IN POLISH
-        changeClassesName(elements_uk, "remove", "hidden");
+        changeClassesName(elements_en, "remove", "hidden");
         changeClassesName(elements_pl, "add", "hidden");
         document.getElementById("slash").className = "hidden";
+        // SET POLISH ALPHABET
+        hangman.alphabet = "abcdefghijklmnopqrstuvwxyz";
         // LOAD ENGLISH WORDS
         loadJSON("./words.json", function(text){
-            hangman.words = JSON.parse(text).words.uk;
+            hangman.words_obj = JSON.parse(text).words.en;
             // START THE GAME
             start();
         });
@@ -285,11 +311,13 @@ function languageSelect() {
 // MAIN FUNCTION
 function start() {
     hangman.reset();
+    hangman.setCategory();
     hangman.setWord();
     hangman.hideWord();
     // CREATE BOARD
     hangman.displayAlphabet();
     hangman.displayHiddenWord();
+    hangman.displayCategory();
     hangman.displayRemainingChances();
     hangman.displayScore();
     // EVENT LISTENERS
